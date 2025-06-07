4cd964bf312ee7796fc507ebaac577feefdd6327
wsjung2023 <145474114+wsjung2023@users.noreply.github.com>
2025-06-07 08:35:18 +0000

    feat: blank dashboard

--- functions/.eslintrc.js
+++ functions/.eslintrc.js
@@ -31,6 +31,7 @@ module.exports = {
     "import/no-unresolved": 0,
     "indent": ["error", 2],
     "@typescript-eslint/no-explicit-any": "off",
+    "@typescript-eslint/no-explicit-any": "off",
     "@typescript-eslint/no-unused-vars": ["warn", { "argsIgnorePattern": "^_" }]
   },
 };
